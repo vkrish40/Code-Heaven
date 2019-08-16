cf039b709e71bc3b078a2b717aa1fdcc889f396e
VARUN KRISHNA <54129236+vkrish40@users.noreply.github.com>
2019-08-16 03:22:19 -0700

    Update kthlargestelementinanarray.java

--- kthlargestelementinanarray.java
+++ kthlargestelementinanarray.java
@@ -6,7 +6,7 @@ class Solution {
     }
 }
 
-/*This can be done in almost linear time as well using quicksort approach (not exactly complete quicksort).
+/*This can be done in almost linear time O(n) as well instead of O(n log n) using quicksort approach (not exactly complete quicksort).
    You'll understand what I am trying to say if you refer this video: 
    https://www.youtube.com/watch?v=hGK_5n81drs
 */
