3c809807db66c88e87e93ae87cf5191d9c4ecd53
VARUN KRISHNA <54129236+vkrish40@users.noreply.github.com>
2019-08-16 03:21:34 -0700

    Update kthlargestelementinanarray.java

--- kthlargestelementinanarray.java
+++ kthlargestelementinanarray.java
@@ -5,3 +5,8 @@ class Solution {
         return nums[(nums.length-k)];
     }
 }
+
+/*This can be done in almost linear time as well using quicksort approach (not exactly complete quicksort).
+   You'll understand what I am trying to say if you refer this video: 
+   https://www.youtube.com/watch?v=hGK_5n81drs
+*/
