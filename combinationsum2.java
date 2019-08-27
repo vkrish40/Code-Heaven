69ac19be015edfd9d81745aa2ac56ed090b36420
VARUN KRISHNA <54129236+vkrish40@users.noreply.github.com>
2019-08-26 23:55:34 -0700

    Add files via upload

---
+++ combinationsum2.java
@@ -0,0 +1,73 @@
+class Solution {
+    
+        List<List<Integer>> full_list=new ArrayList<>();
+    
+        public void diff_subsets(boolean[][] ans, int[] candidates, int sum, int i, List<Integer> list)
+        {
+            if(sum==0)
+            {
+                Collections.sort(list);
+                if(!full_list.contains(list))
+                    full_list.add(list);
+                return;
+            }
+            
+            if(i==0 && ans[i][sum])
+            {
+                list.add(candidates[i]);
+                Collections.sort(list);
+                if(!full_list.contains(list))
+                    full_list.add(list);
+                return;
+            }
+            
+            if(ans[i-1][sum])
+            {
+                List<Integer> temp=new ArrayList<>(list);
+                diff_subsets(ans,candidates,sum,i-1,temp);
+            }
+            if(sum>=candidates[i] && ans[i-1][sum-candidates[i]])
+            {
+                list.add(candidates[i]);
+                diff_subsets(ans,candidates,sum-candidates[i],i-1,list);
+            }                
+                
+        }
+    
+        public void subsetsum(int[] candidates, int target)
+        {
+            boolean[][] ans=new boolean[candidates.length][target+1];
+            for(int i=0;i<candidates.length;i++)
+                ans[i][0]=true;
+            for(int i=0;i<=target;i++)
+            {
+                if(candidates[0]==i)
+                {
+                    ans[0][i]=true;
+                    break;
+                }
+            }
+            for(int i=1;i<candidates.length;i++)
+            {
+                for(int j=1;j<=target;j++)
+                {
+                    if(candidates[i]>j)
+                        ans[i][j]=ans[i-1][j];
+                    else if(ans[i-1][j] || ans[i-1][j-candidates[i]])
+                        ans[i][j]=true;
+                }
+            }
+            
+            if(ans[candidates.length-1][target])
+                diff_subsets(ans, candidates, target, candidates.length-1, new ArrayList<>());
+            else
+                return;
+        }
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        
+        subsetsum(candidates, target);
+        
+        return full_list;
+        
+    }
+}
