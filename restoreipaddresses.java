76fbf2ee3f1d60b2b5e335a8fbafb144d7d29781
VARUN KRISHNA <54129236+vkrish40@users.noreply.github.com>
2019-10-27 15:38:26 -0700

    Add files via upload

---
+++ restoreipaddresses.java
@@ -0,0 +1,39 @@
+class Solution {
+    
+    List<String> ans;
+    
+    public void build(String s, List<Integer> current_list, int start_index, int segment)
+    {
+        if(segment==4 && start_index==s.length())
+        {
+            String temp = current_list.get(0) + "." + current_list.get(1) + "." + current_list.get(2) + "." + current_list.get(3);
+            ans.add(temp);
+            return;
+        }
+        else if(segment==4 || start_index==s.length())
+            return;
+        for(int i=1;i<=3;i++)
+        {
+            if((start_index+i)>s.length())
+                break;
+            int val = Integer.parseInt(s.substring(start_index, start_index+i));
+            if(val>255 || ((i!=1) && s.charAt(start_index)=='0'))
+                continue;
+            current_list.add(val);
+            build(s, current_list, start_index+i, segment+1);
+            current_list.remove(current_list.size()-1);            
+        }
+    }
+    
+    public List<String> restoreIpAddresses(String s) {
+        
+        if(s==null || s.length()==0)
+            return Arrays.asList();
+        
+        ans = new ArrayList<>();
+        
+        build(s, new ArrayList<>(), 0, 0);
+        
+        return ans;   
+    }
+}
